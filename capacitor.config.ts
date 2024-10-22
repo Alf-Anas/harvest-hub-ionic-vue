798b68f18c1d0e5b3ca441ea5619ab37e7c044c5
Alf-Anas <alfadila.anas@gmail.com>
2024-10-22 20:20:25 +0700

    Initial vuetify + ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'harvest.hub.ionic.vue',
+  appName: 'harvest-hub-ionic-vue',
+  webDir: 'dist'
+};
+
+export default config;
